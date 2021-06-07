fafac774b2adb466e45d84c6a1c93243301c9429
TheMshary <themshary94@gmail.com>
2021-06-07 19:00:14 +0300

    Cookie list + data file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import "./App.css";
+import cookies from "./cookies";
+//    arrayName       filename
 
 function App() {
+  const cookieList = cookies.map((cookie) => (
+    <div className="cookie" key={cookie.id}>
+      <img alt={cookie.name} className="cookie-image" src={cookie.image} />
+      <p className="text">{cookie.name}</p>
+      <p className="text">{cookie.price} KD</p>
+    </div>
+  ));
+
   // JSX
   return (
     <div>
-      <h1 className="text">Cookies and Beyond</h1>
-      <h4 className="text">Where cookie maniacs gather</h4>
-      <img
-        id="shop-image"
-        src="https://i.pinimg.com/originals/8f/cf/71/8fcf719bce331fe39d7e31ebf07349f3.jpg"
-      />
+      <div>
+        <h1 className="text">Cookies and Beyond</h1>
+        <h4 className="text">Where cookie maniacs gather</h4>
+        <img
+          id="shop-image"
+          alt="shop"
+          src="https://i.pinimg.com/originals/8f/cf/71/8fcf719bce331fe39d7e31ebf07349f3.jpg"
+        />
+      </div>
+      <div className="list">{cookieList}</div>
     </div>
   );
 }
