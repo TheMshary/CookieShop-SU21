6ecf8607b7c51f2e243f8c966e00429111129cd7
TheMshary <themshary94@gmail.com>
2021-06-28 19:56:29 +0300

    Cookie List API endpoint

--- src/stores/cookieStore.js
+++ src/stores/cookieStore.js
@@ -1,13 +1,24 @@
-import cookies from "../cookies";
 import { makeAutoObservable } from "mobx";
+import axios from "axios";
 
 class CookieStore {
-  cookies = cookies;
+  // receive the cookies data array from the backend
+  // make a web/HTTP request to the server to get the cookies
+  cookies = [];
 
   constructor() {
     // Makes it so that when the data is updated the components will be re-rendered.
     makeAutoObservable(this);
   }
+
+  fetchCookies = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/cookies");
+      this.cookies = response.data;
+    } catch (error) {
+      console.error("fetchCookies: ", error);
+    }
+  };
 
   cookieDelete = (cookieId) => {
     const updatedCookies = this.cookies.filter(
@@ -18,4 +29,6 @@ class CookieStore {
 }
 
 const cookieStore = new CookieStore();
+cookieStore.fetchCookies();
+
 export default cookieStore;
